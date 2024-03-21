a89e42e10a750ff6f4ccdac2a38ab25664f52908
Samuel Le <robbo_rules1@live.com.au>
2024-03-21 12:30:00 +1100

    Added button

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import SearchBar from './Components/SearchBar.js';
+import Button from './Components/Button.js';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <div id="SearchBar">
             <h2 className="title">Search</h2>
             <SearchBar/>
+            <Button />
           </div>
           <div id="Results">
             <h2 className="title">Results</h2>
