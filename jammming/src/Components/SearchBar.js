9625dd6e54699d13e010ec4f6185063ed590f7a7
Samuel Le <robbo_rules1@live.com.au>
2024-04-16 17:44:58 +1000

    Removed button class, added button tag in searchbar

--- jammming/src/Components/SearchBar.js
+++ jammming/src/Components/SearchBar.js
@@ -1,17 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import { getTracks, getTrackInfo } from '../GetAPI.js';
 import '../css/SearchBar.css';
-import Button from './Button.js';
+import '../css/Button.css';
+
+
+function displayResults(searchQuery, accessToken, setTracks)
+{
+    if (searchQuery !== "")
+    {
+        let items = null;
+
+        const response = getTracks(accessToken, searchQuery);
+        response.then((_items) => {
+            items = [..._items];
+            console.log("string", items);
+
+            setTracks(items);
+        });
+    }
+}
 
 function SearchBar({accessToken, setTracks}) {
 
     const [input, setInput] = useState("");
 
     
-
     return (
-        <div>
+        <div class="inputContainer">
             <input id="searchBar" onChange={(event) => setInput(event.target.value)}></input>
-            <Button searchQuery={input} accessToken={accessToken} setTracks={setTracks}/>
+            <button id="button" onClick={() => {displayResults(input, accessToken, setTracks)}}>Go</button>
         </div>
     )
 }
